24b5d67ae0706597ce1d9c55623f77ceeef14166
zhanggw <enjoy524@163.com>
2017-02-09 11:34:16 +0800

    修改
    添加函数choose_best_feature_to_split_withContinuousFeature

--- machine-learning/decisison_tree/main.py
+++ machine-learning/decisison_tree/main.py
@@ -56,8 +56,8 @@ def calculate_gini(data_set):
     return gini
 
 
-def split_data_set(data_set, axis, value):
-    '''抽取出数据集中第axis维中值等于value的行，并且该行中去除掉第axis维
+def split_discrete_data_set(data_set, axis, value):
+    '''对离散变量划分数据集，抽取出数据集中第axis维中值等于value的行，并且该行中去除掉第axis维
 
     :param data_set: 输入数据集
     :param axis: 维度
@@ -70,6 +70,30 @@ def split_data_set(data_set, axis, value):
             reduced_vector = feather_vector[:axis]
             reduced_vector.extend(feather_vector[axis + 1:])
             ret_data_set.append(reduced_vector)
+    return ret_data_set
+
+
+def split_continuous_data_set(data_set, axis, value, direction):
+    '''对连续变量划分数据集
+
+    :param data_set:
+    :param axis:
+    :param value:
+    :param direction: 0，表示大于value；其他表示小于等于value
+    :return:
+    '''
+    ret_data_set = []
+    for feather_vector in data_set:
+        if direction == 0:
+            if feather_vector[axis] > value:
+                reduced_vector = feather_vector[:axis]
+                reduced_vector.extend(feather_vector[axis + 1:])
+                ret_data_set.append(reduced_vector)
+        else:
+            if feather_vector[axis] <= value:
+                reduced_vector = feather_vector[:axis]
+                reduced_vector.extend(feather_vector[axis + 1:])
+                ret_data_set.append(reduced_vector)
     return ret_data_set
 
 
@@ -89,7 +113,7 @@ def choose_best_feature_to_split(dataSet):
         unique_value = set(feature_list)
         new_entropy = 0.0
         for value in unique_value:
-            sub_data_set = split_data_set(dataSet, i, value)
+            sub_data_set = split_discrete_data_set(dataSet, i, value)
             prob = len(sub_data_set) / float(len(dataSet))
             new_entropy += prob * calculate_shannon_entropy(sub_data_set)
         info_gain = base_entropy - new_entropy
@@ -116,7 +140,7 @@ def choose_best_feature_to_split_C45(dataSet):
         new_entropy = 0.0
         split_info = 1.0    # 防止split_info可能为0，这里采用预先加1的方法
         for value in unique_value:
-            sub_data_set = split_data_set(dataSet, i, value)
+            sub_data_set = split_discrete_data_set(dataSet, i, value)
             prob = len(sub_data_set) / float(len(dataSet))
             split_info -= prob * log(prob, 2)
             new_entropy += prob * calculate_shannon_entropy(sub_data_set)
@@ -143,13 +167,79 @@ def choose_best_feature_to_split_gini(dataSet):
         unique_value = set(feature_list)
         newGiniIndex = 0.0
         for value in unique_value:
-            subDataSet = split_data_set(dataSet, i, value)
+            subDataSet = split_discrete_data_set(dataSet, i, value)
             prob = len(subDataSet) / float(len(dataSet))
             newGiniIndex += prob * calculate_gini(subDataSet)
         gini_gain = base_gini - newGiniIndex
         if gini_gain > best_gini_gain:
             best_gini_gain = gini_gain
             best_feature = i
+    return best_feature
+
+
+def choose_best_feature_to_split_withContinuousFeature(dataSet, labels):
+    ''' gini选择最好的特征，会特殊处理连续值的属性
+
+    :param dataSet:
+    :param labels:
+    :return:
+    '''
+    num_feature = len(dataSet[0]) - 1
+    best_feature = -1
+    best_gini_index = inf
+    bestSplitDict = {}
+
+    for i in range(num_feature):
+        feature_list = [example[i] for example in dataSet]
+        unique_value = set(feature_list)
+        # 对连续型特征进行处理
+        if (type(feature_list[0]).__name__ == "int") or (type(feature_list[0]).__name__ == "float"):
+            # 生成n-1个候选划分点
+            sortfeatList = sorted(unique_value)
+            splitList = []
+            for j in range(len(sortfeatList) - 1):
+                splitList.append((sortfeatList[j]+sortfeatList[j+1])/2.0)
+
+            best_split_gini = inf
+            bestSplit = -1
+            for j in range(len(splitList)):
+                value = splitList[j]
+                newGiniIndex = 0.0
+                subDataSet0 = split_continuous_data_set(dataSet, i, value, 0)
+                subDataSet1 = split_continuous_data_set(dataSet, i, value, 1)
+                prob0 = len(subDataSet0)/float(len(dataSet))
+                newGiniIndex += prob0 * calculate_gini(subDataSet0)
+                prob1 = len(subDataSet1) / float(len(dataSet))
+                newGiniIndex += prob1 * calculate_gini(subDataSet1)
+                if newGiniIndex < best_split_gini:
+                    best_split_gini = newGiniIndex
+                    bestSplit = j
+
+            bestSplitDict[labels[i]] = splitList[bestSplit]
+            GiniIndex = best_split_gini
+        # 对离散特征处理
+        else:
+            newGiniIndex = 0.0
+            for value in unique_value:
+                subDataSet = split_discrete_data_set(dataSet, i, value)
+                prob = len(subDataSet) / float(len(dataSet))
+                newGiniIndex += prob * calculate_gini(subDataSet)
+            GiniIndex = newGiniIndex
+
+        if GiniIndex < best_gini_index:
+            best_gini_index = GiniIndex
+            best_feature = i
+
+    # 若当前节点的最佳划分特征为连续特征，则将其以之前记录的划分点为界进行二值化处理，即是否小于等于bestSplitValue
+    if (type(dataSet[0][best_feature]).__name__ == "int") or (type(dataSet[0][best_feature]).__name__ == "float"):
+        bestSplitValue = bestSplitDict[labels[best_feature]]
+        labels[best_feature] = labels[best_feature] + '<=' + str(bestSplitValue)
+        for i in range(shape(dataSet)[0]):
+            if dataSet[i][best_feature] <= bestSplitValue:
+                dataSet[i][best_feature] = 1
+            else:
+                dataSet[i][best_feature] = 0
+
     return best_feature
 
 
@@ -179,7 +269,7 @@ def create_tree(dataSet, labels):
         return majority_cnt(class_list)
 
     #正常循环
-    best_feature = choose_best_feature_to_split_gini(dataSet)
+    best_feature = choose_best_feature_to_split_withContinuousFeature(dataSet, labels)
     best_label = labels[best_feature]
     decision_tree = {best_label: {}}
     del(labels[best_feature])
@@ -188,7 +278,7 @@ def create_tree(dataSet, labels):
     unique_feature_value = set(feature_values)
     for feature_value in unique_feature_value:
         sub_labels = labels[:]
-        decision_tree[best_label][feature_value] = create_tree(split_data_set(dataSet, best_feature, feature_value), sub_labels)
+        decision_tree[best_label][feature_value] = create_tree(split_discrete_data_set(dataSet, best_feature, feature_value), sub_labels)
     return decision_tree
 
 
